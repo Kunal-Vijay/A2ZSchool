17ccdb46617e548c811b5ce993789859948b81cc
kunal <kunal20vijay00@gmail.com>
2024-04-29 19:44:21 +0530

    Beans web scope - @SessionScope Demo

--- src/main/java/com/ktech/a2zschool/service/ContactService.java
+++ src/main/java/com/ktech/a2zschool/service/ContactService.java
@@ -3,11 +3,12 @@ package com.ktech.a2zschool.service;
 import com.ktech.a2zschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
-@RequestScope
+//@RequestScope
+@SessionScope
 public class ContactService {
     private int counter = 0;
 
