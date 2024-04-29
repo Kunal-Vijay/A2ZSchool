bff85bfbf1582d5d90d4a4416c58a89c73a06480
kunal <kunal20vijay00@gmail.com>
2024-04-29 19:24:27 +0530

    Beans web scopes - @RequestScope Demo

--- src/main/java/com/ktech/a2zschool/controller/ContactController.java
+++ src/main/java/com/ktech/a2zschool/controller/ContactController.java
@@ -47,6 +47,8 @@ public class ContactController {
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
+        contactService.setCounter(contactService.getCounter()+1);
+        log.info("Number of times contact form submitted :"+contactService.getCounter());
         return "redirect:/contact";
     }
 
--- src/main/java/com/ktech/a2zschool/service/ContactService.java
+++ src/main/java/com/ktech/a2zschool/service/ContactService.java
@@ -3,11 +3,26 @@ package com.ktech.a2zschool.service;
 import com.ktech.a2zschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Slf4j
 @Service
+@RequestScope
 public class ContactService {
-//    private static Logger log = LoggerFactory.getLogger(ContactService.class);
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public ContactService() {
+        System.out.println("Contact Service bean initialized");
+    }
+
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
 //        TODO - Need to persist the data in DB table
