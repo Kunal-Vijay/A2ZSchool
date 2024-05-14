6ef60decf9dce0e96a17bf074c5d9793a122f998
kunal <kunal20vijay00@gmail.com>
2024-05-14 14:38:31 +0530

    setting up h2 database

--- src/main/java/com/ktech/a2zschool/A2ZSchoolApplication.java
+++ src/main/java/com/ktech/a2zschool/A2ZSchoolApplication.java
@@ -2,9 +2,8 @@ package com.ktech.a2zschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class A2ZSchoolApplication {
 
 	public static void main(String[] args) {
