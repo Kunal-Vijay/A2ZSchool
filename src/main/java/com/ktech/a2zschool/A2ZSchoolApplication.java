2459ec9c52464a4c8b592e53864181c4e2ee4e60
kunal <kunal20vijay00@gmail.com>
2024-05-15 17:45:24 +0530

    Implementing automatic auditing support with Spring Data JPA

--- src/main/java/com/ktech/a2zschool/A2ZSchoolApplication.java
+++ src/main/java/com/ktech/a2zschool/A2ZSchoolApplication.java
@@ -3,11 +3,13 @@ package com.ktech.a2zschool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.ktech.a2zschool.repository")
 @EntityScan("com.ktech.a2zschool.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class A2ZSchoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(A2ZSchoolApplication.class, args);
