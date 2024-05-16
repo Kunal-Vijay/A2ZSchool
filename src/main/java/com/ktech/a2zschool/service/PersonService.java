67d5430fe899da493e76145c3742e62f71c9a542
kunal <kunal20vijay00@gmail.com>
2024-05-16 13:42:10 +0530

    Implement password hashing with BcryptPasswordEncoder

--- src/main/java/com/ktech/a2zschool/service/PersonService.java
+++ src/main/java/com/ktech/a2zschool/service/PersonService.java
@@ -7,6 +7,7 @@ import com.ktech.a2zschool.model.Roles;
 import com.ktech.a2zschool.repository.PersonRepository;
 import com.ktech.a2zschool.repository.RolesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,10 +19,13 @@ public class PersonService {
     @Autowired
     private RolesRepository rolesRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public boolean createNewPerson(Person person){
         boolean isSaved = false;
         Roles role = rolesRepository.getByRoleName(A2ZSchoolContants.STUDENT_ROLE);
         person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
         person = personRepository.save(person);
         if (null != person && person.getPersonId() > 0)
         {
