f003fa8e34841f454bcd0b0f3991a32593e1b990
kunal <kunal20vijay00@gmail.com>
2024-05-16 23:59:06 +0530

    Updating address and person details from Profile page

--- src/main/java/com/ktech/a2zschool/controller/ProfileController.java
+++ src/main/java/com/ktech/a2zschool/controller/ProfileController.java
@@ -1,17 +1,26 @@
 package com.ktech.a2zschool.controller;
 
+import com.ktech.a2zschool.model.Address;
 import com.ktech.a2zschool.model.Person;
 import com.ktech.a2zschool.model.Profile;
+import com.ktech.a2zschool.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class ProfileController {
+    @Autowired
+    PersonRepository personRepository;
     @RequestMapping("/displayProfile")
     public ModelAndView displayMessage(Model model, HttpSession session){
         Person person = (Person) session.getAttribute("loggedInPerson") ;
@@ -29,5 +38,29 @@ public class ProfileController {
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile",profile);
         return modelAndView;
+    }
+
+    @PostMapping(value = "/updateProfile")
+    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors,
+                                HttpSession session)
+    {
+        if(errors.hasErrors()){
+            return "profile.html";
+        }
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        person.setName(profile.getName());
+        person.setEmail(profile.getEmail());
+        person.setMobileNumber(profile.getMobileNumber());
+        if(person.getAddress() ==null || !(person.getAddress().getAddressId()>0)){
+            person.setAddress(new Address());
+        }
+        person.getAddress().setAddress1(profile.getAddress1());
+        person.getAddress().setAddress2(profile.getAddress2());
+        person.getAddress().setCity(profile.getCity());
+        person.getAddress().setState(profile.getState());
+        person.getAddress().setZipCode(profile.getZipCode());
+        Person savedPerson = personRepository.save(person);
+        session.setAttribute("loggedInPerson", savedPerson);
+        return "redirect:/displayProfile";
     }
 }
--- src/main/java/com/ktech/a2zschool/model/Profile.java
+++ src/main/java/com/ktech/a2zschool/model/Profile.java
@@ -28,14 +28,14 @@ public class Profile {
     private String address2;
 
     @NotBlank(message="City must not be blank")
-    @Size(min=5, message="City must be at least 5 characters long")
+    @Size(min=3, message="City must be at least 3 characters long")
     private String city;
 
     @NotBlank(message="State must not be blank")
-    @Size(min=5, message="State must be at least 5 characters long")
+    @Size(min=3, message="State must be at least 3 characters long")
     private String state;
 
     @NotBlank(message="Zip Code must not be blank")
-    @Pattern(regexp="(^$|[0-9]{5})",message = "Zip Code must be 5 digits")
+    @Pattern(regexp="(^$|[0-9]{6})",message = "Zip Code must be 6 digits")
     private String zipCode;
 }
