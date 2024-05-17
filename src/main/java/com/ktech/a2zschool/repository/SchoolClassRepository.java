6cbfa7a67a41cf5dda2468ae3dd8e77218b13ce2
kunal <kunal20vijay00@gmail.com>
2024-05-17 12:23:42 +0530

    Added missed @Repository annotation

--- src/main/java/com/ktech/a2zschool/repository/SchoolClassRepository.java
+++ src/main/java/com/ktech/a2zschool/repository/SchoolClassRepository.java
@@ -2,7 +2,9 @@ package com.ktech.a2zschool.repository;
 
 import com.ktech.a2zschool.model.SchoolClass;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SchoolClassRepository extends JpaRepository<SchoolClass,Integer> {
 
 }
