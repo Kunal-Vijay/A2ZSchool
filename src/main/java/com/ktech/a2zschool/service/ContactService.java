fae4d935e6ad407767826fd6f50b6804fbdf2f2a
kunal <kunal20vijay00@gmail.com>
2024-05-18 10:41:54 +0530

    Implementing Paging and Dynamic Sorting in Messages

--- src/main/java/com/ktech/a2zschool/service/ContactService.java
+++ src/main/java/com/ktech/a2zschool/service/ContactService.java
@@ -5,9 +5,12 @@ import com.ktech.a2zschool.model.Contact;
 import com.ktech.a2zschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -29,9 +32,14 @@ public class ContactService {
         return isSaved;
     }
 
-    public List<Contact> findMsgsWithOpenStatus(){
-        List<Contact> contactMsgs = contactRepository.findByStatus(A2ZSchoolContants.OPEN);
-        return contactMsgs;
+    public Page<Contact> findMsgsWithOpenStatus(int pageNum,String sortField, String sortDir){
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
+                sortDir.equals("asc") ? Sort.by(sortField).ascending()
+                        : Sort.by(sortField).descending());
+        Page<Contact> msgPage = contactRepository.findByStatus(
+                A2ZSchoolContants.OPEN,pageable);
+        return msgPage;
     }
 
     public boolean updateMsgStatus(int contactId){
